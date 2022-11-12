5cd19151970a3b7f58fafcf8b10d20047a1ac0d2
Raimonda <raime.jasaityte@gmail.com>
2022-11-12 18:19:58 +0200

    atlikta ketvirta užduotis

--- 4 task.py
+++ 4 task.py
@@ -9,3 +9,25 @@
 
 # Metodas: 
 # was_expensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins True, kitu atveju False.
+
+class Movie:
+  def __init__(self, title, director, budget):
+    self.title = title
+    self.director = director
+    self.budget = budget
+
+  def __repr__(self):
+    return f'(Pavadinimas: {self.title}, Režisierius: {self.director}, Biudžetas: {self.budget} USD)'  
+
+  def was_expensive(self):
+    if self.budget > 100000000:
+      return True
+    else:
+      return False
+
+movie1 = Movie('Šrekas', 'Andrew Adamson', 60000000)
+movie2 = Movie('Hobitas', 'Peter Jackson', 7000000000)
+
+print(movie1, movie2)
+print(movie1.was_expensive())
+print(movie2.was_expensive())
