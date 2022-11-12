b8f949004f1824c656e0624a016808bb4b7a4705
Raimonda <raime.jasaityte@gmail.com>
2022-11-12 18:09:02 +0200

    first commit

---
+++ 3 task.py
@@ -0,0 +1,4 @@
+# 3 task
+# Turimas "audi" dict.
+
+# Parašykite funkciją "show_object_values", kuri kaip argumentą priims objektą ir grąžins visus jo "values" list'e.
---
+++ 5 task.py
@@ -0,0 +1,15 @@
+# 5 task
+
+# Importuokite reikiamus modulius, kad veiktų žemiau nurodytos funkcijos
+
+# Kitų failų ir žemiau esančio kodo nekeiskite
+
+a = composition(one, four);
+b = division(four, two);
+c = subtraction(three, two);
+d = multiplication(five, two);
+
+print(a);
+print(b);
+print(c);
+print(d);
